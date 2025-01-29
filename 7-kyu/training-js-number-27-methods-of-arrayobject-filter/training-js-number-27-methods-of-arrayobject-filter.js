169b0f7c574b1199bbf90390ce7e711f0ab62b8d
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-01-29 12:08:12 +0700

    Add solution - CodeHub

---
+++ 7-kyu/training-js-number-27-methods-of-arrayobject-filter/training-js-number-27-methods-of-arrayobject-filter.js
@@ -0,0 +1,10 @@
+function countGrade(scores) {
+    return {
+        S: scores.filter(score => score === 100).length,
+        A: scores.filter(score => score < 100 && score >= 90).length,
+        B: scores.filter(score => score < 90 && score >= 80).length,
+        C: scores.filter(score => score < 80 && score >= 60).length,
+        D: scores.filter(score => score < 60 && score >= 0).length,
+        X: scores.filter(score => score === -1).length
+    };
+}
