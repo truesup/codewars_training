3f42843e8fdfe5ea53a57fab8a982a3b5b8d8954
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-02-07 13:04:40 +0700

    Add solution - CodeHub

---
+++ 7-kyu/training-js-number-28-methods-of-arrayobject-every-and-some/training-js-number-28-methods-of-arrayobject-every-and-some.js
@@ -0,0 +1,15 @@
+function mirrorImage(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      String(arr[i]) ===
+      String(arr[i + 1])
+        .split('')
+        .reverse()
+        .join('')
+    ) {
+      return [arr[i], arr[i + 1]];
+    }
+  }
+​
+  return [-1, -1];
+}
