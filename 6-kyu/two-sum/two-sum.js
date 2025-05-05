84ea32232968e924f4a8fede7894525599b4e34e
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-05-05 20:36:50 +0700

    Add solution - CodeHub

---
+++ 6-kyu/two-sum/two-sum.js
@@ -0,0 +1,11 @@
+function twoSum(numbers, target) {
+  const map = new Map();
+​
+  for (let i = 0; i < numbers.length; i++) {
+    const complement = target - numbers[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(numbers[i], i);
+  }
+}
