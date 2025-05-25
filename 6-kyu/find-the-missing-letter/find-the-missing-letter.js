0de3fc0aedb8491507c20836181ebc38c6066da8
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-05-25 17:09:56 +0700

    Add solution - CodeHub

---
+++ 6-kyu/find-the-missing-letter/find-the-missing-letter.js
@@ -0,0 +1,11 @@
+function findMissingLetter(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    const currentCode = array[i].charCodeAt(0);
+    const nextCode = array[i + 1].charCodeAt(0);
+​
+    if (nextCode !== currentCode + 1) {
+      return String.fromCharCode(currentCode + 1);
+    }
+  }
+  return ' '; 
+}
