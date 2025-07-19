1d202775f57928699ddf3ff804e2799bce1ac01a
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-07-19 18:05:56 +0800

    Add solution - CodeHub

---
+++ 7-kyu/row-weights/row-weights.js
@@ -0,0 +1,14 @@
+function rowWeights(array) {
+  let team1 = 0;
+  let team2 = 0;
+​
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+      team1 += array[i]; 
+    } else {
+      team2 += array[i]; 
+    }
+  }
+​
+  return [team1, team2];
+}
