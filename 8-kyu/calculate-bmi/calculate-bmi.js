246ce620479d2466937e7dc8dd63f33f8f47d1e9
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-07-25 21:20:18 +0800

    Add solution - CodeHub

---
+++ 8-kyu/calculate-bmi/calculate-bmi.js
@@ -0,0 +1,8 @@
+function bmi(weight, height) {
+  const bmi = weight / (height * height);
+​
+  if (bmi <= 18.5) return "Underweight";
+  if (bmi <= 25.0) return "Normal";
+  if (bmi <= 30.0) return "Overweight";
+  return "Obese";
+}
