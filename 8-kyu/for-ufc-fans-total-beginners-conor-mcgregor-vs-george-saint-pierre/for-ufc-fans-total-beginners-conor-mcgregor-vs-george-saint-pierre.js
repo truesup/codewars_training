f3188f80f17dc9bcf03ab695138ce7784bbb7eeb
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-07-26 17:09:41 +0800

    Add solution - CodeHub

---
+++ 8-kyu/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre.js
@@ -0,0 +1,7 @@
+function quote(fighter) {
+  if (fighter.toLowerCase() === 'george saint pierre') {
+    return "I am not impressed by your performance.";
+  } else if (fighter.toLowerCase() === 'conor mcgregor') {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+  }
+}
