e68729dd1937bfb884ba92a3608a5753935512c8
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-08-26 18:18:34 +0800

    Add solution - CodeHub

---
+++ 7-kyu/basic-calculator/basic-calculator.js
@@ -0,0 +1,10 @@
+function calculate(num1, operation, num2) {
+  switch (operation) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    case '/': return num2 !== 0 ? num1 / num2 : null;
+    default: return null;
+  }
+}
+​
