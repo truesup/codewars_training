b1dde283f59d7851df50356b07aef2d723dc21b1
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-08-30 13:58:16 +0800

    Add solution - CodeHub

---
+++ 5-kyu/rot13-1/rot13-1.js
@@ -0,0 +1,7 @@
+function rot13(message) {
+  return message.replace(/[a-zA-Z]/g, (char) => {
+    const base = char <= 'Z' ? 65 : 97;
+    return String.fromCharCode(((char.charCodeAt(0) - base + 13) % 26) + base);
+  });
+}
+​
