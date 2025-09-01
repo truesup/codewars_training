7101ab1b6fca6bedcf73829c1747075340ef7156
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-09-01 16:43:25 +0800

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-create-the-rooms/grasshopper-create-the-rooms.js
@@ -0,0 +1,18 @@
+var rooms = {
+  kitchen: {
+    name: "Kitchen",
+    description: "A messy kitchen with dirty dishes and a locked fridge.",
+    completed: false
+  },
+  library: {
+    name: "Library",
+    description: "A dusty library filled with ancient books and a hidden passage.",
+    completed: false
+  },
+  basement: {
+    name: "Basement",
+    description: "A dark basement with strange noises and a locked chest.",
+    completed: false
+  }
+};
+​
