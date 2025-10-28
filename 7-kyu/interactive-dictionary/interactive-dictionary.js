f624f36df1f9600324cd2ea1795a773ec80d5989
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-10-28 14:12:17 +0800

    Add solution - CodeHub

---
+++ 7-kyu/interactive-dictionary/interactive-dictionary.js
@@ -0,0 +1,14 @@
+class Dictionary {
+  constructor() {
+    this.entries = {};
+  }
+  
+  newEntry(key, value) {
+    this.entries[key] = value;
+  }
+  
+  look(key) {
+    return this.entries[key] || `Can't find entry for ${key}`;
+  }
+}
+​
