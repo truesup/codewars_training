37c0aabf6147a27f22c693782b2b3f1256bb18aa
Pavel M. <154729420+truesup@users.noreply.github.com>
2025-11-11 14:07:18 +0800

    Add solution - CodeHub

---
+++ 7-kyu/find-the-calculation-type/find-the-calculation-type.js
@@ -0,0 +1,7 @@
+function calcType(a, b, res) {
+  if (a + b === res) return "addition"
+  if (a - b === res) return "subtraction"
+  if (a * b === res) return "multiplication"
+  if (a / b === res) return "division"
+}
+​
